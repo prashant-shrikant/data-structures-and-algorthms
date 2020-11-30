a865c5cf12e8daf4533df856f0316397cde5da2f
prashant shrikant <prashantshrikant@mac.local>
2020-11-29 18:46:33 -0800

    O(n) solution

--- multiple-pointers/sumZero.js
+++ multiple-pointers/sumZero.js
@@ -2,7 +2,7 @@
 // The function should find the first pair where the sum is 0. 
 // Return an array that includes both values that sum to zero or undefined if a pair does not exist
 
-
+// naive solution
 const sumZero = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
@@ -17,4 +17,22 @@ const result = sumZero([-4, -3, -2, 2, 6, 7])
 console.log(result)
 
 
+// Optimized solution using multiple pointers
+const sumZeroMultiplePointers = arr => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        if (arr[left] + arr[right] == 0) {
+            return [arr[left], arr[right]];
+        } else if (arr[left] + arr[right] > 0) {
+            right--;
+        } else {
+            left++
+        }
+    }
+}
+
+const resultOptimized = sumZeroMultiplePointers([-4, -3, -2, 2, 6, 7]);
+console.log(resultOptimized);
 
