b18ed662dba34e5bec902d8f04f27b99e26707fc
prashant shrikant <prashantshrikant@mac.local>
2020-12-02 20:58:59 -0800

    naive solution using two for loops

--- sliding-window/maxSumSubArray.js
+++ sliding-window/maxSumSubArray.js
@@ -7,15 +7,19 @@
 // Naive solution
 const maxSubarraySum = (arr, n) => {
     let max = 0
+    if (n > arr.length ) {
+        return null;
+    }
     for (let i = 0; i < arr.length - n + 1; i++) {
-        for (let j = i+1; j < i + n; i++) {
-            const sum = arr[i] + arr[j];
-            if (sum > max) {
-                max = sum;
-            }
+        let sum = 0;
+        for (let j = 0; j < n; j++) {
+            sum = sum + arr[i + j];
+        }
+        if (sum > max) {
+            max = sum;
         }
     } return max;
 }
 
-const result = maxSubarraySum([1,2,5,2,8,1,5],2);
+const result = maxSubarraySum([1,2,5,2,8,1,5],4);
 console.log(result);
