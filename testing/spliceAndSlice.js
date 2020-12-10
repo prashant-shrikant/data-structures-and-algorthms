16693e02de7b1ed42ec519a34de678c32c698d7f
prashant shrikant <prashantshrikant@mac.local>
2020-12-09 21:54:17 -0800

    isSubsequence using iterative approach

---
+++ testing/spliceAndSlice.js
@@ -0,0 +1,14 @@
+const spliceAndSlice = () => {
+    const arr1 = [1, 2, 3, 4, 5, 6];
+    const arr2 = [1, 2, 3, 4, 5, 6];
+const a = 'Prashant Shrikant';
+a.substring(1, 8)
+
+    console.log(arr1.splice(3, 2));
+    console.log(arr2.slice(1, 3))
+
+    console.log(arr1);
+    console.log(arr2);
+}
+
+spliceAndSlice();
