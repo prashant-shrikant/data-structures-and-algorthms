14956d5e91227e051ec52c9f089f6f65d749dab8
prashant shrikant <prashantshrikant@mac.local>
2020-12-14 08:53:53 -0800

    recursively sorting the data

--- sorting-algorithms/QuickSort.js
+++ sorting-algorithms/QuickSort.js
@@ -19,7 +19,13 @@ const swap = (arr, first, second) => {
     return arr;
 }
 
-
+const quickSort = (arr, left=0, right=arr.length -1 ) => {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        quickSort(arr, left, pivotIndex-1);
+        quickSort(arr, pivotIndex+1, right);
+    } return arr;
+}
 
 const result = quickSort([4, 8, 2, 1, 5, 7, 5, 6, 3]);
 console.log(result);
