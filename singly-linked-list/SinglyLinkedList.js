7ac07284c5405142282e721abe659b2b2204ae41
prashant shrikant <prashantshrikant@mac.local>
2020-12-15 08:36:26 -0800

    popping the last node

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -23,8 +23,40 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if(!this.head) {
+            return undefined;
+        } else {
+            let current = this.head;
+            let newTail = current;
+            while (current.next) {
+                newTail = current;
+                current = current.next;
+            } 
+            this.tail = newTail;
+            this.tail.next = null;
+            this.length--;
+            if (this.length === 0) {
+                this.head = null;
+                this.tail = null;
+            }
+            return current;
+        }
+    }
+    traverse() {
+        let current = this.head;
+        while (current) {
+            console.log(current.val);
+            current = current.next;
+        }
+    }
 }
 
 const list = new SinglyLinkedList();
 list.push("prashant");
-list.push("shrikant")
+list.push("shrikant");
+list.push("usha")
+list.push("shweta")
+list.traverse();
+list.pop();
+list.traverse();
