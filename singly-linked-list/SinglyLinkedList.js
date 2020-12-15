074def8af95f1cd53f36a3dd1847af4bfadde394
prashant shrikant <prashantshrikant@mac.local>
2020-12-15 07:55:26 -0800

    structure

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -11,16 +11,5 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
-    push(val) {
-        if (this.head === null) {
-            this.head = val;
-            this.tail = val;
-            length++;
-        } else if (this.head == this.tail) {
-            this.tail = val;
-            length++;
-        } else {
-            this.
-        }
-    }
+    
 }
