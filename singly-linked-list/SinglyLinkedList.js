83ae75f380360ac7bcf6ded72420bc1f3581c141
prashant shrikant <prashantshrikant@mac.local>
2020-12-15 08:02:59 -0800

    push method

--- singly-linked-list/SinglyLinkedList.js
+++ singly-linked-list/SinglyLinkedList.js
@@ -11,5 +11,20 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
-    
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+
+const list = new SinglyLinkedList();
+list.push("prashant");
+list.push("shrikant")
