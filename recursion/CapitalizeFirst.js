3599a5dfda92f985f40a36a627d686f292faa86d
prashant shrikant <prashantshrikant@mac.local>
2020-12-25 21:46:52 -0800

    capitalize first alphabet

--- recursion/CapitalizeFirst.js
+++ recursion/CapitalizeFirst.js
@@ -1,18 +1,18 @@
 // captitalize first letter of every item in the array
 
-const capitalizeWords = array => {
+const capitalizeFirst = array => {
   if (array.length === 1) {
     const word = array[0];
     array[0] = word[0].toUpperCase() + word.substring(1);
     console.log(array)
     return array;
   }
-  let res = capitalizeWords(array.slice(0, -1));
+  let res = capitalizeFirst(array.slice(0, -1));
   const newArr = array.slice(array.length - 1);
   const newWord = newArr[0];
   res.push(newWord[0].toUpperCase() + newWord.substring(1));
   return res;
 }
 
-const result = capitalizeWords(['prashant', 'shrikant', 'pacific'])
+const result = capitalizeFirst(['prashant', 'shrikant', 'pacific'])
 console.log(result);
